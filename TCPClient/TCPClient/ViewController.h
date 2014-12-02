7b74f528474ff24f084aab455f26fc8bf2f3b935
Fire2Sky <hackerbyty@gmail.com>
2014-12-02 18:20:00 +0800

    更新TCPClinet文件

--- TCPClient/TCPClient/ViewController.h
+++ TCPClient/TCPClient/ViewController.h
@@ -7,9 +7,25 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreFoundation/CoreFoundation.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 9000
 
 @interface ViewController : UIViewController
+{
+	//操作标志  0 为发送,1 为接受
+	int flag;
+}
+//定义属性输入流,与CFReadStreamRef相对应
+@property (nonatomic, retain) NSInputStream * inputStream;
+//定义属性输出流,与CFWriteStreamRef相对应
+@property (nonatomic, retain) NSOutputStream * outputStream;
 
+@property (weak, nonatomic) IBOutlet UILabel * message;
+-(IBAction)sendData:(id)sender;
+-(IBAction)receiveData:(id)sender;
 
 @end
 
--- TCPClient/TCPClient/ViewController.m
+++ TCPClient/TCPClient/ViewController.m
@@ -24,4 +24,12 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (void)initNetworkCommunication
+{
+	CFReadStreamRef readStream;
+	CFWriteStreamRef writeStream;
+	CFStream   
+
+}
 @end
