87807ba529819908fbd1aa3d1e67ff663e84ce56
Fire2Sky <hackerbyty@gmail.com>
2014-12-03 18:34:59 +0800

    更新了TCPClient的代码

--- TCPClient/TCPClient/ViewController.m
+++ TCPClient/TCPClient/ViewController.m
@@ -24,4 +24,80 @@
     // Dispose of any resources that can be recreated.
 }
 
+//初始化网络的方法
+- (void)initNetworkCommunication
+{
+    CFReadStreamRef readStream;
+    CFWriteStreamRef writeStream;
+    CFStreamCreatePairWithSocketToHost(NULL, (CFStringRef)@"192.168.1.103",PORT, &readStream, &writeStream);
+    _inputStream = (_bridge_transfer NSInputStream *)readStream;
+    _outputStream = (_bridge_transfer NSOutputStream *)writeStream;
+    [_inputStream setDelegate:self];
+    [_outputStream setDelegate:self];
+    
+    [_inputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    [_outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    
+    [_inputStream open];
+    [_outputStream open];
+    
+}
+
+//单击"发送"按钮
+- (IBAction)sendData:(id)sender
+{
+    flag = 0;
+    [self initNetworkCommunication];
+    
+}
+
+
+//单击"接受"按钮
+- (IBAction)receiveData:(id)sender
+{
+    flag = 1;
+    [self initNetworkCommunication];
+    
+}
+
+-(void)stream:(NSStream *)theStream handleEvent:(NSStreamEvent)streamEvent
+{
+    NSString *event;
+    switch (streamEvent)
+    {
+        case NSStreamEventNone: event = @"NSStreamEventNone";
+            break;
+        case NSStreamEventOpenCompleted: event = @"NSSteamEventOpenCompleted";
+            break;
+        case NSStreamEventHasBytesAvailable: event = @"NSStreamEventHasBytesAvailable";
+            if (flag == 1 && theStream == _inputStream)
+            {
+                NSMutableData *input = [[NSMutableData alloc] init];
+                uint8_t buffer[1024];
+                long len;
+                while([_inputStream hasBytesAvailable])
+                {
+                    len = [_inputStream read:buffer maxLength:sizeof(buffer)];
+                    if (len > 0)
+                    {
+                        [input appendBytes:buffer length:len];
+                        
+                    }
+                    
+                }
+                NSString * resultstring = [[NSString alloc] initWithData: input encoding: NSUTF8StringEncoding];
+                NSLog(@"接受: %@", resultstring);
+                _message.text = resultstring;
+            }
+            break;
+        case NSStreamEventHasSpaceAvailable: event = @"NSStreamEventHasSpaceAvailable";
+            if (flag == 0 && theStream == _outputStream)
+            {
+                //输出
+                UInt8 buff[] = "Hello Server!";
+                
+                
+            }
+}
+
 @end
